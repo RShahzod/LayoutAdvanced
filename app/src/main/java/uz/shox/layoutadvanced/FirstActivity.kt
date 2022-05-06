6f3c7a0b8da8a6aacdd8b47d9466b00b49a9086c
Shahzod <rshoxzodr@gmail.com>
2022-05-06 12:50:30 +0500

    up first

--- app/src/main/java/uz/shox/layoutadvanced/FirstActivity.kt
+++ app/src/main/java/uz/shox/layoutadvanced/FirstActivity.kt
@@ -2,10 +2,24 @@ package uz.shox.layoutadvanced
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.CornerFamily
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        val imageShap = findViewById<ShapeableImageView>(R.id.imageShapeable)
+
+        imageShap.setOnClickListener {
+            val radius = resources.getDimension(R.dimen.corner)
+            val shapeAppearanceModel = imageShap.shapeAppearanceModel.toBuilder()
+                .setAllCorners(CornerFamily.CUT, radius)
+                .build()
+            imageShap.shapeAppearanceModel = shapeAppearanceModel
+        }
+
     }
 }
